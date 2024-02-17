b9cf2a1621c45948e89cbdcc5ae6203714bd0b5d
Your Name <you@example.com>
2024-02-17 17:34:04 -0300

    33dasdas

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,6 +59,10 @@ export default function Page() {
     setShowModal(false);
   };
 
+  const searchSize = (e: any) => {
+    setSearch(e.target.value)
+  };
+
   const getPages = () => {
     const pages = []
     const halfDisplayedPages = Math.floor(5 / 2)
@@ -104,6 +108,8 @@ export default function Page() {
         setCurrentPage(1)
       )
     );
+    setPostsPerPage(posts.length);
+
   }, [search, posts]);
 
   useEffect(() => {
@@ -138,138 +144,148 @@ export default function Page() {
 
 
   return (
+
     <div className="container" style={{ backgroundColor: 'lightgray', color: 'black' }}>
-      <header style={{ backgroundColor: 'black', textAlign: 'center', padding: '10px' }}>
-        <img src="/icons/icon.svg" width="120" height="" alt="Descrição da imagem" />
-        <h5 style={{ color: 'white' }}>{posts.length}</h5>
-      </header>
-      <input type="text" className="form-control" value={search} onChange={e => setSearch(e.target.value)} placeholder="Buscar..." />
-      {loading ? (
-        <div className="table-responsive">
-          <table className="table table-dark">
-            <thead>
-              <tr>
-                <th className="placeholder-glow">
-                  <span className="placeholder col-6"></span>
-                </th>
-                <th className="placeholder-glow">
-                  <span className="placeholder col-6"></span>
-                </th>
-                <th className="placeholder-glow">
-                  <span className="placeholder col-6"></span>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {Array(postsPerPage).fill(0).map((_, index) => (
-                <tr key={index}>
-                  <td className="placeholder-glow">
-                    <span className="placeholder col-6"></span>
-                  </td>
-                  <td className="placeholder-glow">
-                    <span className="placeholder col-6"></span>
-                  </td>
-                  <td className="placeholder-glow">
-                    <span className="placeholder col-6"></span>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <nav>
-            <div className='d-flex justify-content-center'>
-              <ul className="pagination mx-auto">
-                <li className="page-item ">
-                  <a className="page-link" onClick={() => setCurrentPage(currentPage > 1 ? currentPage - 1 : currentPage)}>
-                    <span aria-hidden="true">&laquo;</span>
-                  </a>
-                </li>
-                {getPages().map((page: number, index: number) => (
-                  <li key={index} className={`page-item ${page === currentPage ? 'active' : ''}`}>
-                    <a className="page-link" onClick={() => setCurrentPage(page)}>{page}</a>
-                  </li>
-                ))}
-                <a className="page-link">?</a>
+      <main>
 
-                <li className="page-item">
-                  <a className="page-link" onClick={() => setCurrentPage(currentPage < Math.ceil(filteredPosts.length / postsPerPage) ? currentPage + 1 : currentPage)}>
-                    <span aria-hidden="true">&raquo;</span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </div>
-      ) : (
-        <div className="table-responsive">
-          <table className="table bootstrap-table" style={{ backgroundColor: 'gray', color: 'black', border: '1px solid black' }}>
-            <thead style={{ backgroundColor: 'black', color: 'white' }}>
-              <tr>
-                <th data-sortable="true" onClick={() => sortPosts('ID')}>ID</th>
-                <th data-sortable="true" onClick={() => sortPosts('Titulo')}>Titulo</th>
-                <th data-sortable="true" onClick={() => sortPosts('Artista')}>Artista</th>
-              </tr>
-            </thead>
-            <tbody>
-              {currentPosts.map((post: { ID: string; Titulo: string; Artista: string; }) => (
-                <tr key={post.ID} onClick={() => openModal({ ID: post.ID, Titulo: post.Titulo, Artista: post.Artista })}>
-                  <td>{post.ID}</td>
-                  <td className="text-truncate" style={{ maxWidth: '150px' }} title={post.Titulo}>{post.Titulo}</td>
-                  <td className="text-truncate" style={{ maxWidth: '150px' }} title={post.Artista}>{post.Artista}</td>
-                </tr>
-              ))}
-            </tbody>
-            <Modal show={showModal} onHide={closeModal}>
-              <Modal.Header closeButton>
-                <Modal.Title>{currentPostId.ID} - {currentPostId.Titulo} - {currentPostId.Artista}</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <div style={{ width: '100%', height: '100%', backgroundColor: 'lightgray' }}>
-                  <iframe title="deezer-widget" src={currentLink} width="100%" height="150" frameBorder="0" allow="encrypted-media; clipboard-write"></iframe>
+        <header className="fixed-top" style={{ backgroundColor: 'black', textAlign: 'center', padding: '10px' }}>
+          <img src="/icons/icon.svg" width="80" height="" alt="Descrição da imagem" />
+          <h6 style={{ color: 'white', fontSize: '10px' }}>{posts.length}</h6>
+        </header>
+
+        <div style={{ paddingTop: '90px', paddingBottom: '70px' }} className="container" >
+
+          <input type="text" className="form-control" value={search} onChange={e => searchSize(e)} placeholder="Buscar..." />
+          {loading ? (
+            <div className="table-responsive">
+              <table className="table table-dark">
+                <thead>
+                  <tr>
+                    <th className="placeholder-glow">
+                      <span className="placeholder col-6"></span>
+                    </th>
+                    <th className="placeholder-glow">
+                      <span className="placeholder col-6"></span>
+                    </th>
+                    <th className="placeholder-glow">
+                      <span className="placeholder col-6"></span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Array(postsPerPage).fill(0).map((_, index) => (
+                    <tr key={index}>
+                      <td className="placeholder-glow">
+                        <span className="placeholder col-6"></span>
+                      </td>
+                      <td className="placeholder-glow">
+                        <span className="placeholder col-6"></span>
+                      </td>
+                      <td className="placeholder-glow">
+                        <span className="placeholder col-6"></span>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <nav>
+                <div className='d-flex justify-content-center'>
+                  <ul className="pagination mx-auto">
+                    <li className="page-item ">
+                      <a className="page-link" onClick={() => setCurrentPage(currentPage > 1 ? currentPage - 1 : currentPage)}>
+                        <span aria-hidden="true">&laquo;</span>
+                      </a>
+                    </li>
+                    {getPages().map((page: number, index: number) => (
+                      <li key={index} className={`page-item ${page === currentPage ? 'active' : ''}`}>
+                        <a className="page-link" onClick={() => setCurrentPage(page)}>{page}</a>
+                      </li>
+                    ))}
+                    <a className="page-link">?</a>
+
+                    <li className="page-item">
+                      <a className="page-link" onClick={() => setCurrentPage(currentPage < Math.ceil(filteredPosts.length / postsPerPage) ? currentPage + 1 : currentPage)}>
+                        <span aria-hidden="true">&raquo;</span>
+                      </a>
+                    </li>
+                  </ul>
                 </div>
-              </Modal.Body>
-            </Modal>
-          </table>
-          <nav>
-            <div className='d-flex justify-content-center'>
-              <ul className="pagination mx-auto">
-                <li className="page-item ">
-                  <a className="page-link" onClick={() => setCurrentPage(currentPage > 1 ? currentPage - 1 : currentPage)}>
-                    <span aria-hidden="true">&laquo;</span>
-                  </a>
-                </li>
-                {getPages().map((page: number, index: number) => (
-                  <li key={index} className={`page-item ${page === currentPage ? 'active' : ''}`}>
-                    <a className="page-link" onClick={() => setCurrentPage(page)}>{page}</a>
-                  </li>
-                ))}
-                <li className="page-item">
-                  <a className="page-link" onClick={() => setCurrentPage(currentPage < Math.ceil(filteredPosts.length / postsPerPage) ? currentPage + 1 : currentPage)}>
-                    <span aria-hidden="true">&raquo;</span>
-                  </a>
-                </li>
-              </ul>
+              </nav>
             </div>
-          </nav>
+          ) : (
+            <div className=" table-responsive">
+              <table className="table bootstrap-table" style={{ backgroundColor: 'gray', color: 'black', border: '1px solid black' }}>
+                <thead style={{ backgroundColor: 'black', color: 'white' }}>
+                  <tr>
+                    <th data-sortable="true" onClick={() => sortPosts('ID')}>ID</th>
+                    <th data-sortable="true" onClick={() => sortPosts('Titulo')}>Titulo</th>
+                    <th data-sortable="true" onClick={() => sortPosts('Artista')}>Artista</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {currentPosts.map((post: { ID: string; Titulo: string; Artista: string; }) => (
+                    <tr key={post.ID} onClick={() => openModal({ ID: post.ID, Titulo: post.Titulo, Artista: post.Artista })}>
+                      <td>{post.ID}</td>
+                      <td className="text-truncate" style={{ maxWidth: '150px' }} title={post.Titulo}>{post.Titulo}</td>
+                      <td className="text-truncate" style={{ maxWidth: '150px' }} title={post.Artista}>{post.Artista}</td>
+                    </tr>
+                  ))}
+                </tbody>
+                <Modal show={showModal} onHide={closeModal}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>{currentPostId.ID} - {currentPostId.Titulo} - {currentPostId.Artista}</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <div style={{ width: '100%', height: '100%', backgroundColor: 'lightgray' }}>
+                      <iframe title="deezer-widget" src={currentLink} width="100%" height="150" frameBorder="0" allow="encrypted-media; clipboard-write"></iframe>
+                    </div>
+                  </Modal.Body>
+                </Modal>
+              </table>
+              <nav>
+                <div className='d-flex justify-content-center'>
+                  <ul className="pagination mx-auto">
+                    <li className="page-item ">
+                      <a className="page-link" onClick={() => setCurrentPage(currentPage > 1 ? currentPage - 1 : currentPage)}>
+                        <span aria-hidden="true">&laquo;</span>
+                      </a>
+                    </li>
+                    {getPages().map((page: number, index: number) => (
+                      <li key={index} className={`page-item ${page === currentPage ? 'active' : ''}`}>
+                        <a className="page-link" onClick={() => setCurrentPage(page)}>{page}</a>
+                      </li>
+                    ))}
+                    <li className="page-item">
+                      <a className="page-link" onClick={() => setCurrentPage(currentPage < Math.ceil(filteredPosts.length / postsPerPage) ? currentPage + 1 : currentPage)}>
+                        <span aria-hidden="true">&raquo;</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
 
+            </div>
+          )}
         </div>
-      )}       <footer className="bg-dark text-white py-3">
-        <Container>
-          <Row>
-            <Col xs={6}>
-              <a href="https://www.instagram.com/k.rezende59?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer" className="text-white">
-                <FaInstagram size={24} />
-              </a>
-              <a href="https://wa.me/+5512974067360" target="_blank" rel="noopener noreferrer" className="text-white ml-2">
-                <FaWhatsapp size={24} />
-              </a>
-            </Col>
-            <Col xs={6} className="text-end">
-              Direitos reservados AutoPyWeb
-            </Col>
-          </Row>
-        </Container>
-      </footer>
+        <footer className="fixed-bottom bg-dark text-white py-3">
+          <Container>
+            <Row>
+              <Col xs={6}>
+                <a href="https://www.instagram.com/k.rezende59?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer" className="text-white">
+                  <FaInstagram size={24} />
+                </a>
+                <a href="https://wa.me/+5512974067360" target="_blank" rel="noopener noreferrer" className="text-white ml-2">
+                  <FaWhatsapp size={24} />
+                </a>
+              </Col>
+              <Col xs={6} className="text-end">
+                2024©AutoPyWeb
+              </Col>
+            </Row>
+          </Container>
+        </footer>
+      </main>
+
       <style jsx>{`
         .page-item.active .page-link {
           background-color: black;
