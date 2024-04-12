92354bcab36156f9af61267cfb5c5dd3634cdee1
José dos Santos Junior <junior.ilha@hotmail.com.br>
2024-04-12 23:29:37 +0000

    Atualização do google Sheets

--- pages/api/listmusic.ts
+++ pages/api/listmusic.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 
 type ResponseData = {
-  musicList: Array<{ID: string, codigo: string, Artista: string, Titulo: string}>
+  musicList: Array<{ ID: string, codigo: string, Artista: string, Titulo: string }>
 }
 
 export default async function handler(
@@ -16,17 +16,20 @@ export default async function handler(
   const sheets = google.sheets({ version: 'v4', auth: process.env.GOOGLE_KEY });
   const response = await sheets.spreadsheets.values.get({
     spreadsheetId: process.env.GOOGLE_SHEETS_ID,
-    range: 'Musicas {1}', // Nome da sua aba
+    range: process.env.GOOGLE_SHEETS_RANGE, // Nome da sua aba
   });
 
-  const rows: any  = response.data.values;
+  const rows: any = response.data.values;
   if (rows.length) {
-    const musicList = rows.slice(1).map((row : any) => ({
-      ID: uuidv4(), // Gera um ID único
-      codigo: row[1], // Adiciona o valor original de row[0] para o campo "codigo"
-      Artista: row[0],
-      Titulo: row[4],
-    }));
+    const musicList = rows.slice(1)
+      .filter((row: any) => row.length > 0 && row.every((cell: any) => cell.trim() !== ''))
+      .map((row: any) => ({
+        ID: uuidv4(), // Gera um ID único
+        codigo: row[0], // Adiciona o valor original de row[0] para o campo "codigo"
+        Artista: row[1],
+        Titulo: row[2],
+      }));
+
 
     res.status(200).json({ musicList });
   } else {
